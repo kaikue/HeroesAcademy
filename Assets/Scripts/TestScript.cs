af545b6134a43ed9ad1566c340273e607d0b400e
Kai Kuehner <kvk@pobox.com>
2016-09-24 21:44:50 -0400

    Commit unity project

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Loaded!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
