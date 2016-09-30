bcf961f8cfe0bc6abf8bb1d19edca8c1ca5e00da
Kai Kuehner <kvk@pobox.com>
2016-09-30 18:49:06 -0400

    Add jumping

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,23 +3,37 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 
-	private Rigidbody2D rigidbody;
-	private float speed = 0.1f;
+	private Rigidbody2D rigidBody;
+	private float speed = 20;
+	private float jumpHeight = 5;
+	private float groundHeight;
 
-	// Use this for initialization
 	void Start () {
-		rigidbody = gameObject.GetComponent<Rigidbody2D> ();
+		rigidBody = gameObject.GetComponent<Rigidbody2D> ();
+		groundHeight = gameObject.GetComponent<PolygonCollider2D>().bounds.size.y / 2 + 0.05f;
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow))
-		{
-			rigidbody.MovePosition(rigidbody.position - new Vector2 (speed, 0));
+		//left/right movement
+		bool left = Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow);
+		bool right = Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow);
+		if (left && !right) {
+			rigidBody.AddForce (new Vector2 (-speed, 0));
+		} else if (right && !left) {
+			rigidBody.AddForce (new Vector2 (speed, 0));
+		} else {
+			//slow to a stop
+			rigidBody.velocity = new Vector2 (rigidBody.velocity.x * 0.75f, rigidBody.velocity.y);
 		}
-		if(Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow))
+
+		//jump
+		bool up = Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow);
+		RaycastHit2D[] collisions = Physics2D.RaycastAll(gameObject.transform.position, Vector2.down, groundHeight);
+		bool onGround = collisions.Length > 1; //TODO improve
+		if(up && onGround)
 		{
-			rigidbody.MovePosition(rigidbody.position + new Vector2 (speed, 0));
+			//TODO sometimes this applies twice for some reason
+			rigidBody.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
 		}
 	}
 }
