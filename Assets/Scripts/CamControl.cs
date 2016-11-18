cb168d5bd5b57a324f2f66ed9590cbf0b3f063bb
mbofenka <mbofenka@andrew.cmu.edu>
2016-11-18 16:18:12 -0500

    New level and more
    Made obstacle course level, fixed ground check, disabled negative health

---
+++ Assets/Scripts/CamControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamControl : MonoBehaviour {
+
+	public GameObject player;
+
+	public float leftBound;
+	public float rightBound;
+	public float upBound;
+	public float downBound;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//position camera to center on player
+		float xPos = player.transform.position.x;
+		xPos = Mathf.Clamp (xPos, leftBound, rightBound);
+		float yPos = player.transform.position.y;
+		yPos = Mathf.Clamp (yPos, downBound, upBound);
+		float zPos = this.transform.position.z;
+		Vector3 pos = new Vector3 (xPos, yPos, zPos);
+		this.transform.position = pos;
+	
+	}
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -13,56 +13,80 @@ public class PlayerScript : MonoBehaviour {
 
 	private Rigidbody2D rigidBody;
 	private float speed = 20;
-	private float jumpHeight = 5;
+	private float jumpHeight = 7;
+	private float maxSpeed = 8;
 	private float groundHeight;
+	private float playerWidth;
 	private bool dir;
 	private float pressedTime;
 
 	void Start () {
 		rigidBody = gameObject.GetComponent<Rigidbody2D> ();
-		groundHeight = gameObject.GetComponent<PolygonCollider2D>().bounds.size.y / 2 + 0.05f;
+		groundHeight = gameObject.GetComponent<Collider2D>().bounds.size.y / 2 + 0.05f;
+		playerWidth = gameObject.GetComponent<Collider2D> ().bounds.size.x / 2;
 		dir = LEFT;
 		health = 100;
 		updateHealthText();
 	}
 	
 	void Update () {
-		//left/right movement
-		bool left = Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow);
-		bool right = Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow);
-		if (left && !right) {
-			rigidBody.AddForce (new Vector2 (-speed, 0));
-			dir = LEFT;
-		} else if (right && !left) {
-			rigidBody.AddForce (new Vector2 (speed, 0));
-			dir = RIGHT;
-		} else {
-			//slow to a stop
-			rigidBody.velocity = new Vector2 (rigidBody.velocity.x * 0.75f, rigidBody.velocity.y);
-		}
 
 		//jump
 		bool up = Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.UpArrow);
 		//this currently only checks directly under the player's center, need to check all positions to the sides too
 		bool onGround = Physics2D.OverlapPoint (new Vector2(gameObject.transform.position.x, gameObject.transform.position.y) + Vector2.down * groundHeight) != null;
+		if (!onGround)
+			onGround = Physics2D.OverlapPoint (new Vector2(gameObject.transform.position.x + playerWidth, gameObject.transform.position.y) + Vector2.down * groundHeight) != null;
+		if (!onGround)
+			onGround = Physics2D.OverlapPoint (new Vector2(gameObject.transform.position.x - playerWidth, gameObject.transform.position.y) + Vector2.down * groundHeight) != null;
 		if(up && onGround)
 		{
 			rigidBody.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
 		}
+
+		//left/right movement
+		bool left = Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow);
+		bool right = Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow);
+		if (left && !right) {
+//			if (onGround) {
+				rigidBody.AddForce (new Vector2 (-speed, 0));
+				dir = LEFT;
+//			}
+		} else if (right && !left) {
+//			if (onGround) {
+				rigidBody.AddForce (new Vector2 (speed, 0));
+				dir = RIGHT;
+//			} 
+		} else {
+			//slow to a stop
+			rigidBody.velocity = new Vector2 (rigidBody.velocity.x * 0.75f, rigidBody.velocity.y);
+		}
+
+		//fix to not exceed max speed
+
+		float currSpeed = rigidBody.velocity.x;
+		currSpeed = Mathf.Clamp (currSpeed, -maxSpeed, maxSpeed);
+		rigidBody.velocity = new Vector2 (currSpeed, rigidBody.velocity.y);
 
 		//hold space to spawn couch
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			pressedTime = Time.time;
 		}
 		if (Input.GetKeyUp (KeyCode.Space)) {
-			int timeDiff = (int)((Time.time - pressedTime) * 10);
-			print (timeDiff);
-			hurt (timeDiff);
+			if (health > 1) {
+				int timeDiff = (int)((Time.time - pressedTime) * 10) + 1;
+				//adjust couch trajectory if health is too low
+				if (health < timeDiff)
+					timeDiff = health - 1;
+				print (timeDiff);
+				hurt (timeDiff);
+			}
 		}
 	}
 
 	void hurt(int damage) {
 		Vector3 couchpos = gameObject.transform.position;
+		couchpos.z += 0.1f; //keep player sprite in front of couch sprite
 		Vector3 offset = (dir == LEFT ? Vector3.left : Vector3.right) * 2;
 		couchpos += offset;
 		GameObject spawned = ((Transform)Instantiate(couch, couchpos, Quaternion.identity)).gameObject;
