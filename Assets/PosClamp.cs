659583135869cdc4d7eaa2c9bfa92cdd76fb80ed
mbofenka <mbofenka@andrew.cmu.edu>
2016-12-08 13:15:17 -0500

    Game Icon Added

---
+++ Assets/PosClamp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PosClamp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float x = Mathf.Clamp (this.transform.position.x, 23f, Mathf.Infinity);
+		this.transform.position = new Vector2 (x, this.transform.position.y);
+	
+	}
+}
