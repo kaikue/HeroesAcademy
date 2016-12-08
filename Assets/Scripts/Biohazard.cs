4c74ba2dfeaf375330a9dfc034a23661ac8877c9
mbofenka <mbofenka@andrew.cmu.edu>
2016-12-08 01:38:42 -0500

    Made the biohazard in the air duct dangerous

---
+++ Assets/Scripts/Biohazard.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Biohazard : MonoBehaviour {
+
+	public int damage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+
+		if (other.tag == "Player")
+			other.GetComponent<PlayerScript> ().hurt (damage, false);
+
+	}
+
+}
