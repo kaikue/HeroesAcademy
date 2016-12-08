e425096c88cdc70ce33dbcd795341bf78f1c29c3
mbofenka <mbofenka@andrew.cmu.edu>
2016-12-07 23:47:18 -0500

    Enemies + Their Animations

---
+++ Assets/Scripts/EnemySpawnTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawnTrigger : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject enemy;
+
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+
+		if (other.gameObject == player)
+			enemy.SetActive (true);
+
+	}
+	
+
+}
